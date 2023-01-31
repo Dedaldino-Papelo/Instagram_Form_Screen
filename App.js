05b8f2b794f3bf75c84bf865a2a3a43348a2e16e
Dedaldino-Papelo <kelsonpapelo18@gmail.com>
2023-01-31 22:51:55 +0000

    added navigation with stack navigation

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { View } from 'react-native';
 import Login from './components/Login';
 import Register from './components/Register'
 import { globalStyle } from './global/style';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <View style={globalStyle.container}>
-      <Register />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Login' component={Login} />
+          <Stack.Screen name='Register' component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
