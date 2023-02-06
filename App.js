f211569b20e733645aa20aaaa7d3729f30c463d6
Dedaldino-Papelo <kelsonpapelo18@gmail.com>
2023-02-06 23:25:05 +0000

    get the data from the storage

--- App.js
+++ App.js
@@ -34,7 +34,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      initialRouteName='Home'
         screenOptions={{
           headerStyle: {
             backgroundColor: "#F0faff"
--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,40 @@
-import { Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { globalStyle } from "../global/style";
 
 export default function Home(){
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        getData()
+    },[])
+
+    const getData = async() => {
+       try {
+        
+        const Username = await AsyncStorage.getItem('Username')
+        
+        if(Username !== null){
+            setName(Username)
+        }
+
+       } catch (error) {
+        console.log(error)
+       }
+    }
 
     return(
         <View style={globalStyle.container}>
-            <Text>Welcome</Text>
+            <Text style={style.text}>Welcome {name}</Text>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    text:{
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: 'center'
+    }
+})
